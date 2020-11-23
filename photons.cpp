caebb6f703b49a1f73b02039967a6bb01fcb79f0
Rafael <rafaelhuntstokes@gmail.com>
2020-11-23 14:59:19 +0000

    Tidied code comments and fixed intercepts

--- photons.cpp
+++ photons.cpp
@@ -9,6 +9,7 @@
 #include "TVector3.h"
 #include "Math/Vector3D.h"
 #include "TRandom.h"
+#include "TRandom3.h"
 #include "TMath.h"
 #include "TCanvas.h"
 #include "TROOT.h"
@@ -38,7 +39,6 @@ class Event {
     float pi = TMath::Pi();
     vector < TVector3 > gamma_dirs; // array of vectors, each element is a polar 3D vector  
     vector < TVector3 > intercepts; // array of vectors, each element is photon/sphere intercept coords
-    //vector <float> event_position;
 
     // experimenting with using ROOT 3D physics vectors instead of C++
     TVector3 event_position; 
@@ -46,29 +46,18 @@ class Event {
     // constructor of class randomly populates the energy and position attributes, converts E --> # gammas and 
     // specifies a random direction for the photons 
     public: 
-        Event(){
-            // initialise the random number generator using the system time seed so it's different each time 
-            //srand(time(NULL));
-
-            // randomly assign an energy between 1 --> 5MeV 
-            // rand() is defined in <cstdlib> and generates a random number from 0 --> RAND_MAX constant 
-            // taking RAND_MAX % 5 is number from 0 --> 4, so add 1 for 1 --> 5. 
-            //energy = 1 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX) / 4);   
-
+        Event(){ 
             // ROOT random number generator 
-            // define a new TRandom class type via a pointer r1 
-            TRandom *r1 = new TRandom(time(NULL));
+            // define a new TRandom class type via a pointer r1
+            // zero seed - random number each time called  
+            TRandom3 *r1 = new TRandom3(0);
 
             // get energy from uniform distribution between 0-->5MeV
             energy = r1->Uniform(1, 5); 
             
             printf("The energy from ROOT is: %f\n", energy);
-            // generate random phi between 0 --> 2pi
-            // generate random theta between 0 --> pi  
-            // generate random R between 0 --> 6
-            // float phi   = rand() / (static_cast <float> (RAND_MAX) / (2 * 3.141592653589793238463)); 
-            // float theta = rand() / (static_cast <float> (RAND_MAX) / 3.141592653589793238463); 
-            // float R     = rand() / (static_cast <float> (RAND_MAX) / 6); 
+            
+            // randomly produce the event position coordinates in spherical polars
             float phi = r1->Uniform(0, 2*pi);
             float theta = r1->Uniform(0,pi);
             float R = r1->Uniform(0,6);
@@ -78,12 +67,6 @@ class Event {
 
             // print the vector to the terminal nicely for sanity checking 
             printf("The vector [x, y, z] is: [");
-            
-            // empty array with 3 elements 
-            double d[3];
-
-            // fill d-array with coordinates of event
-            //event_position.GetCoordinates(d);
             for (int i = 0; i < 3; i++){
                 if (i < 2){
                     printf("%f ", event_position[i]);
@@ -99,75 +82,45 @@ class Event {
             // assign a direction to each gamma
             gammaDir();
 
-            // print all the unit photon direction vectors 
-            // for (auto i : gamma_dirs){
-            //     double d[3]; 
-            //     i.GetCoordinates(d);
-            //     for (auto j : d){
-            //         cout << j << " ";
-            //     };
-            //     cout << endl; 
-            // };
-
             // find intercept of photon direction and PMT sphere 
             // create an array of ROOT 3D polar vectors which store the photon/PMT intercepts
             // and calculate the time of flight and store in a C-array 
-            //ROOT::Math::Polar3DVector intercepts[num_photons]; 
             float time_of_flight[num_photons]; 
             gammaIntercept(time_of_flight); 
 
+            // print out the calculated intercepts 
             puts("INTERCEPTS AT: \n");
             for (int i =0; i < num_photons; i++){
-                float temp[3];
-                //intercepts[i].GetCoordinates(temp);
                 printf("intercept %d at: [ %f, %f, %f ]\n", i, intercepts[i][0], intercepts[i][1], intercepts[i][2]);
-            }
-
-            // checking 
-            // printf("returning the origin within the event\n");
-            // double *origin = get_origin(); 
-            // for (int i = 0; i < 3; i++){
-            //     printf("\nloopin #%d\n", i);
-            //     printf("%f, ", *origin);
-            //     ++origin;
-
-            // };
-            // printf("\nFINISH\n"); 
+            };
         };
 
         int convertGammas(){
             // convert to a number of photons given the event energy
             int photons_per_mev = 5;  
-
             const int num_photons = energy * photons_per_mev; 
-            //const int num_photons = 10;
             printf("Number of photons is: %d\n", num_photons);
-
+            
             return num_photons;
         };
 
         void gammaDir(){
-            //vector <float> gamma_direction; 
             TVector3 gamma_direction; 
-            TRandom *r1 = new TRandom(time(NULL));
+            TRandom *r1 = new TRandom(0);
 
+            // for each photon generate a direction vector
             for(int i =0; i < num_photons; i++){
-                // create random r, theta, phi relative to event location
-                // float r     = 1.0; // doesn't really matter for the direction vector
-                // float theta = rand() / (static_cast <float> (RAND_MAX) / M_PI);
-                // float phi   = rand() / (static_cast <float> (RAND_MAX) / (2 * M_PI)); 
-                
                 float phi = r1->Uniform(0, 2*pi);
                 float theta = r1->Uniform(0,pi);
                 float r = r1->Uniform(0,6);
-                // populate vector and normalise
-                // float vec_size = sqrt (pow(r,2) + pow(theta,2) + pow(phi,2));
-                // gamma_direction = {r/vec_size, theta/vec_size, phi/vec_size};
 
+                // convert from spherical --> cartesian coords for easy time later
                 gamma_direction.SetXYZ(r*sin(theta)*cos(phi), r*sin(theta)*sin(phi), r*cos(theta));
+
+                // convert to unit vector 
                 gamma_direction = gamma_direction.Unit(); 
 
-                // add vector to the containing array for the event 
+                // add vector to the containing vector for the event 
                 gamma_dirs.push_back(gamma_direction);
             };
         }; 
@@ -179,90 +132,49 @@ class Event {
             for loop, so it's pointing at the next element of intercepts for assignment. The maths for the intercept is 
             taken from wikipedia sphere/line intercept page. */
 
-            TVector3 intercept;  // intercept of 1 photon with PMT sphere surface 
-            TVector3 centre;     // centre of sphere (detector) and coordinate system
-            TVector3 diff;       // vector difference between event origin and sphere centre 
-            float d;                              // distance along line from event origin to intercept  
-            float tof;                            // time of flight 
-            float del;                            // huge chunk of equation I'd previously forgotten to add...
+            TVector3 intercept;   // intercept of 1 photon with PMT sphere surface 
+            TVector3 centre;      // centre of sphere (detector) and coordinate system
+            TVector3 diff;        // vector difference between event origin and sphere centre 
+            float d;              // distance along line from event origin to intercept  
+            float tof;            // time of flight 
+            float del;            // huge chunk of equation I'd previously forgotten to add...
 
-            centre.SetXYZ(0,0,0);
+            centre.SetXYZ(0,0,0); // center of containing sphere is the origin
+
             // loop over all the photons 
-            int flag = 1; 
             for (auto i : gamma_dirs){
-                diff = event_position - centre;
-                
-                del = pow((i.Dot(diff)), 2) - (diff.Mag2() - 36);
-                if (flag == 1){
-                    cout << "dir vec is: "<< endl;
-                    for (int k = 0; k < 3; k++){
-                        cout << "[";
-                        cout << i[k] << ",";
 
-                    };
-                    cout << endl; 
-                    cout << "Magnitude of allegeded unit vector is: " << i.Mag() << endl;
-                    flag = 2; 
-                };
+                // mathematics of intercept from wikipedia 
+                diff = event_position - centre;
+                del = pow((i.Dot(diff)), 2) - (diff.Mag2() - 36);
+
+                // there are two solutions, a forwards and a backwards one +-del 
                 float d1 = - (i.Dot(diff)) + sqrt(del);
                 float d2 = - (i.Dot(diff)) - sqrt(del); 
-                printf("del is: %f\nd1: %f \nd2: %f", del, d1, d2);
 
-                double checker = (event_position + d1*i -centre).Mag();
-                printf("\nChecker is %f", checker); 
-                if (checker == 6){
+                // take the positive value solution as the correct one
+                if (d1 > 0){
                     d = d1;
-                    puts("\n USING d1!\n");
                 } else{
-                    d = d2; 
-                    puts("\n USING d2!\n");
-
+                    d = d2;
                 };
                  
                 // intercept is event_origin + ( unit dir vec * d )
-                TVector3 intercept1 = event_position + (d1 * i);  
-                TVector3 intercept2 = event_position + (d2 * i); 
-
-                cout << "\nINT1 r = " << sqrt(pow(intercept1[0],2) + pow(intercept1[1],2) +pow(intercept1[2], 2)) << endl; 
-                cout << "INT2 r = " << sqrt(pow(intercept2[0],2) + pow(intercept2[1],2) +pow(intercept2[2], 2)) << endl; 
-                //printf("\nint1 is: %f\nint2 is: %f ", intercept1[0], intercept2[0]);
-                intercepts.push_back(intercept1); 
-                //intercepts.push_back(intercept2); 
-                //intercept[0] = 6;
+                TVector3 intercept = event_position + (d * i);  
+                intercepts.push_back(intercept); 
                 
-
-                //printf("Distance to sphere surface: %f\n", d); 
-
                 // calculate time of flight and assign to correct array element
                 tof = d / pow(3,8); 
                 *time_of_flights = tof; 
 
-                
-                
-
-                // print out the intercept 
-                // float d[3];
-                // intercept.GetCoordinates(d);
-                // cout << "Intercept: "; 
-                // for (auto j : d){
-                //     cout << j << " ";
-                // };
-                // cout << endl;  
-
-                // add intercept of photon to intercepts list via pointer
-                //*intercepts = intercept; 
-
                 // increment pointer to point at next element of intercepts
-                //++intercepts; 
                 ++time_of_flights;   
             };
         };
 
         double * get_origin(){
-
             // MUST BE STATIC for this to work!
             static double x[3];
-            //event_position.GetCoordinates(x); 
 
             // puts("INSIDE GET ORIGIN!");
             for (int i = 0; i < 3; i++){
@@ -273,13 +185,10 @@ class Event {
         };
 
         double * get_intercepts(int number){
-
             // weird...pass the intercept number in a loop in sphere, and this function 
             // will return a pointer to an array containing the intercept of that photon
-            //puts("INTERCEPT AT: \n");
             static double temp[3];
-            //intercepts[number].GetCoordinates(temp);
-            //printf("intercept %d at: [ %f, %f, %f ]\n", i, temp[0], temp[1], temp[2]);
+            
             for (int i = 0; i < 3; i++){
                 temp[i] = intercepts[number][i];
             };
--- sphere.cpp
+++ sphere.cpp
@@ -10,6 +10,77 @@
 #include <math.h>
 #include <array>
 using namespace std; 
+void plot_event(int num_events){
+    
+    // initialise a list of Event objects of a given number
+    Event *events = new Event[num_events];
+    for (int i = 0; i < num_events; i++){
+        double * origin = events[i].get_origin();
+        int num_photons =  events[i].get_num_photons(); 
+        cout << "NUMBER OF INTERCEPT POINTS RETURNED " << num_photons << endl; 
+
+        // container for all the photon-sphere intercepts
+        double cart1[num_photons][3]; 
+
+        // print the photon-sphere intercepts 
+        cout << "The photon intercepts are: " << endl;
+
+        // loop over each photon-sphere intercept
+        for (int l = 0; l < num_photons; l++){
+        
+            // obtain singlular photon-sphere intercept
+            double * intercept = events[i].get_intercepts(l); 
+        
+            // populate container with intercepts
+            cart1[l][0] = intercept[0]; 
+            cart1[l][1] = intercept[1]; 
+            cart1[l][2] = intercept[2];
+
+            // print the photon-sphere intercepts 
+            cout << "[";
+            for (int k =0; k < 3; k++){
+                cout << cart1[l][k];
+                if (k == 2){
+                    cout << "]" << endl;
+                } else {
+                    cout << ", ";
+                };
+            }; 
+        };
+
+        cout << "Event Origin passed: ";
+        for (int j = 0; j < 3; j++){
+            cout << origin[j] << " ";
+        };
+        cout << endl;
+
+        // loop over every photon intercept
+        // create a pointer to an empty array that will store all of the TPoly3D lines 
+        TPolyLine3D *trajectories[num_photons];
+        for (int d = 0; d < num_photons; d++){
+            // array stores the coordinates of 1 point
+            double coords[3]; 
+
+            // add a line to the array of lones 
+            trajectories[d] = new TPolyLine3D(2);
+            TPolyMarker3D *point = new TPolyMarker3D(1, cart1[d], 3);
+            trajectories[d]->SetPoint(0, origin[0], origin[1], origin[2]);
+            trajectories[d]->SetPoint(1, cart1[d][0], cart1[d][1], cart1[d][2]); 
+            trajectories[d]->SetLineColor(i+1);
+            point->SetMarkerColor(i+1);
+            // draw the line 
+            trajectories[d]->Draw();
+            point->Draw();
+        };
+
+        // create a PolyPoint to mark event origin 
+        TPolyMarker3D *pm3d2 = new TPolyMarker3D(1, origin, 5);
+        pm3d2->Draw();
+        };
+
+    // garbage collection 
+    delete[] events;
+};
 
 void sphere(){
 
@@ -22,119 +93,30 @@ void sphere(){
     // geometry manager for sphere
     gSystem->Load("libGeom");
     new TGeoManager("world", "the simplest geometry");
+
+    // these don't matter for this purpose but need to be defined 
     TGeoMaterial *mat = new TGeoMaterial("Vacuum",0,0,0);
     TGeoMedium   *med = new TGeoMedium("Vacuum",1,mat);
+
+    // create sphere of defined medium with r[0->6], theta[0->180], phi[0->360]
     TGeoVolume *top=gGeoManager->MakeSphere("Top",med,0.,6,0, 180., 0., 360.);
     gGeoManager->SetTopVolume(top);
+
+    // always close geomtery after creation to allow it to be drawn
     gGeoManager->CloseGeometry();
     top->SetLineColor(kMagenta);
+
+    // default to invisible, so set sphere visible  
     gGeoManager->SetTopVisible();
+
+    // draw sphere
     top->Draw();
 
-    Event ev1;
-    double * origin = ev1.get_origin();
-    int num_photons =  ev1.get_num_photons(); 
-    cout << "NUMBER OF INTERCEPT POINTS RETURNED " << num_photons << endl; 
-
-    // container for all the photons
-    double cart1[num_photons][3]; 
-    for (int i = 0; i < num_photons; i++){
-        // obtain photon-sphere intercept
-        double * intercept = ev1.get_intercepts(i); 
-        
-        // convert to cartesian 
-        cart1[i][0] = intercept[0]; //* sin(intercept[1]) * cos(intercept[2]);
-        cart1[i][1] = intercept[1]; //* sin(intercept[1]) * sin(intercept[2]);
-        cart1[i][2] = intercept[2]; //* cos(intercept[1]); 
-
-        
-    };
-    //int s_array = cart1.size();
-    //cout << "Number of points plotted " << s_array << endl; 
-    cout << "The photon intercepts are: " << endl;
-    for (int j =0; j < num_photons;  j++){
-        cout << "[";
-        for (int k =0; k < 3; k++){
-            cout << cart1[j][k] << " "; 
-        }; 
-        cout << "]"; 
-        cout << endl; 
-    };
-
-    // convert to cartesian coordinates 
-    float cart[3]; 
-    cart[0] = origin[0]; //* sin(origin[1]) * cos(origin[2]);
-    cart[1] = origin[1]; //* sin(origin[1]) * sin(origin[2]);
-    cart[2] = origin[2]; //* cos(origin[1]); 
-
-    cout << "Event Origin passed: ";
-    for (int j = 0; j < 3; j++){
-        cout << cart[j] << " ";
-    };
-    cout << endl;
-
-    // loop over every photon intercept
-
-    // create a pointer to an empty array that will store all of the TPoly3D lines 
-    TPolyLine3D *trajectories[num_photons];
-    for (int i = 0; i < num_photons; i++){
-        // array stores the coordinates of 1 point
-        double coords[3]; 
-        
-        // loop over each coordinate
-        for (int j = 0; j < 3; j++){
-            coords[j] = cart1[i][j]; 
-        }; 
-
-        // draw a line from the origin to the intercept
-        // float r = sqrt(pow(coords[0], 2) + pow(coords[1], 2) + pow(coords[2], 2));
-        // cout << "r is  " << r << endl; 
-
-        // add a line to the array of lones 
-        trajectories[i] = new TPolyLine3D(2);
-        TPolyMarker3D *point = new TPolyMarker3D(1, coords, 3);
-        trajectories[i]->SetPoint(0, cart[0], cart[1], cart[2]);
-        trajectories[i]->SetPoint(1, cart1[i][0], cart1[i][1], cart1[i][2]); 
-        // pl3d->SetLineWidth(1);
-        // pl3d->SetLineColor(2);
-        // if (i == 0){
-        //     pl3d->SetPoint(1, 1, 2, 3);
-        //     pl3d->SetPoint(2, cart[0], cart[1], cart[2]);
-        //     cout << cart[0] << " " << cart[1] << " " << cart[2] << endl; 
-        // pl3d->Draw();
-        // point->Draw();
-        // };
-
-        // draw the line 
-        trajectories[i]->Draw();
-        point->Draw();
-
-        //delete pl3d;
-    };
-
-
-
-    // // create a PolyPoint to mark event origin 
-    TPolyMarker3D *pm3d2 = new TPolyMarker3D(1, cart, 5);
-    pm3d2->Draw();
-    // // create  a 3D polyline with  
-    // TPolyLine3D *pl3d1 = new TPolyLine3D(2, cart);
-    // TPolyMarker3D *posPhoton = new TPolyMarker3D(1, cart1, 3);
-    
-
-    // // line width and colour attributes 
-    // pl3d1->SetLineWidth(1);
-    // pl3d1->SetLineColor(2);
-
-    // // draw
-    
-    // pl3d1->Draw(); 
-    
-    // posPhoton->Draw(); 
-
+    // now GENERATE EVENTS 
+    int num_events = 1;
+    plot_event(num_events); 
 };
 
-int main(){
-     
+int main(){ 
     sphere();
 }; 
